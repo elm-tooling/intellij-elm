e6ef5d82235c6e3aad5674b80fbca68fac783ac1
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-02-26 23:27:27 +0100

    New: Use SVG not PNG for icons

--- src/main/kotlin/org/elm/ide/icons/ElmIcons.kt
+++ src/main/kotlin/org/elm/ide/icons/ElmIcons.kt
@@ -7,21 +7,21 @@ import javax.swing.Icon
 object ElmIcons {
 
     /** Basic file icon, matching the rest of IntelliJ's file icons */
-    val FILE = getIcon("elm-file.png")
+    val FILE = getIcon("elm-file-32x32.svg")
 
     /** Colorful Elm icon */
-    val COLORFUL = getIcon("elm-colorful.png")
+    val COLORFUL = getIcon("elm-colorful-original.svg")
 
     /** Gutter icon for values and types exposed by an Elm module */
-    val EXPOSED_GUTTER = getIcon("elm-exposure.png")
+    val EXPOSED_GUTTER = getIcon("elm-exposure-original.svg")
 
     val RECURSIVE_CALL = AllIcons.Gutter.RecursiveMethod
 
     // STRUCTURE VIEW ICONS
 
-    val FUNCTION = getIcon("function.png")
-    val UNION_TYPE = getIcon("type.png")
-    val TYPE_ALIAS = getIcon("type.png")
+    val FUNCTION = getIcon("function-32x32.svg")
+    val UNION_TYPE = getIcon("type-32x32.svg")
+    val TYPE_ALIAS = getIcon("type-32x32.svg")
 
     private fun getIcon(path: String): Icon {
         return IconLoader.getIcon("/icons/$path", ElmIcons::class.java)
