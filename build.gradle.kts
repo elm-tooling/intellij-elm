f1d9285b948865ac8085511c702e96fc1540ef6d
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-02-26 23:30:02 +0100

    Fixed: Plugin verifier now works

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
+import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -53,6 +55,20 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         name = properties("pluginName")
+    }
+
+    pluginVerification {
+        ides {
+            // Earliest and latest stable IC releases from the current compatibility window.
+            ide(properties("platformType"), "2024.3.7", useInstaller = false)
+            ide(properties("platformType"), "2025.3.3", useInstaller = false)
+            // In CI, also verify against the current EAP line.
+            if (providers.environmentVariable("CI").orNull == "true") {
+                select {
+                    channels.set(listOf(ProductRelease.Channel.EAP))
+                }
+            }
+        }
     }
 }
 
@@ -129,6 +145,7 @@ tasks {
     withType<KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(JvmTarget.fromTarget(javaVersion))
+            jvmDefault.set(JvmDefaultMode.NO_COMPATIBILITY)
         }
     }
 
