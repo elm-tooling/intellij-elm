8bd382ba155247d89897a29b2d2c5b06e82ed0a2
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-03-05 16:58:28 +0100

    Remove "until build"
    An update to the intellij platform has deprecated this. This may be the cause of not being able to load the plugin from a build artifact.

--- build.gradle.kts
+++ build.gradle.kts
@@ -192,7 +192,6 @@ tasks {
         // intellij.updateSinceUntilBuild.set(false)
 
         sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
