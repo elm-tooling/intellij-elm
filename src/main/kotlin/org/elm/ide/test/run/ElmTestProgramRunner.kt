37e2fbdbc4643a756e22326a7b8d12c5c0747004
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-03-06 12:08:26 +0100

    Fix Elm test runner settings serialization crash path

--- src/main/kotlin/org/elm/ide/test/run/ElmTestProgramRunner.kt
+++ src/main/kotlin/org/elm/ide/test/run/ElmTestProgramRunner.kt
@@ -34,11 +34,11 @@ class ElmTestProgramRunner : GenericProgramRunner<Settings>() {
 
 class Settings : RunnerSettings {
     override fun writeExternal(element: Element?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // No custom runner settings to persist.
     }
 
     override fun readExternal(element: Element?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // No custom runner settings to restore.
     }
 
 }
